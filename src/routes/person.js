c05fc52ce62622a5715cc631e5faebc015afef05
JuanPabloVerduzcoCampos <90339360+JuanPabloVerduzcoCampos@users.noreply.github.com>
2023-03-30 16:03:08 -0600

    Practica 16

---
+++ src/routes/person.js
@@ -0,0 +1,13 @@
+// Importamos el módulo 'express'
+let express = require('express');
+
+// Creamos una instancia de un objeto Router de Express
+let router = express.Router();
+
+// Definimos una ruta para manejar solicitudes GET a la ruta '/person'
+router.get('/person', (req, res) => {
+    res.send('haz solicitado un listado de personas');
+});
+
+// Exportamos el objeto Router para que pueda ser utilizado en otro lugar de la aplicación
+module.exports = router;
